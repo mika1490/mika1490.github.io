c8c7c78377c77f6a21692214813cb731347ce123
Mikaela Krantman <mikaelakrantman@Mikaelas-MBP.lan>
2021-01-03 12:18:54 -0800

    made mobile view edits

--- js/main.js
+++ js/main.js
@@ -6,11 +6,63 @@ $(document).ready(function() {
     $('.about').addClass('visible');
     $('.project-section').addClass('visible');
     // This toggles the hamburger icon menu and alters the background colors for focus
-    $(".myIcon-toggle").click(function(){
-        $(this).toggleClass("active")
-        // $('body').toggleClass("active")
+    $(".myIcon-toggle-about").click(function(){
+        $('.navbar-collapse').toggle();
+        $('.myIcon-toggle').toggleClass("active")
         $('.page-content').toggleClass("active")
+    });
+    $(".myIcon-toggle-location").click(function(){
+        let navLocation = window.location.href;
+        $('.navbar-collapse').toggle();
+        $('.myIcon-toggle').toggleClass("active")
+        $('.page-content').toggleClass("active")
+        location.replace(navLocation);
+        return false;
+    });
+    // This function will navigate to #projects from the collapsed navbar view
+    $(".projects-link").click(function(){
+        $('.myIcon-toggle').toggleClass("active");
+        $('.navbar-collapse').toggle();
+        if ($('.page-content').is(":hidden")){
+            $('.page-content').toggleClass("active");
+        }
+        location.replace('http://mikadumont.com/#projects');
+        return false;
     })
+    $(".about-link").click(function(){
+        // $('.navbar-collapse').toggle();
+        // $('.myIcon-toggle').toggleClass("active");
+        // if ($('.page-content').is(":hidden")){
+        //     $('.page-content').toggleClass("active");
+        // }
+        location.replace('http://mikadumont.com/about.html');
+        return false;
+    })
+    $(".memoji").click(function(){
+        if ($(".navbar-collapse").is(":visible")){
+            $('.navbar-collapse').hide();
+            $('.myIcon-toggle').toggleClass("active");
+        }
+        if ($(".page-content").is(":hidden")){
+            $('.page-content').toggleClass("active");
+        }
+
+        location.replace('http://mikadumont.com/index.html#home');
+        return false;
+    })
+    // $(".memoji-about").click(function(){
+    //     if ($(".navbar-collapse").is(":visible")){
+    //         $('.navbar-collapse').hide();
+    //         $('.myIcon-toggle').toggleClass("active");
+    //     }
+    //     if ($(".page-content").is(":hidden")){
+    //         $('.page-content').toggleClass("active");
+    //     }
+
+    //     location.replace('http://127.0.0.1:5501/about.html#home');
+    //     return false;
+    // })
+    
     // The scroll event will run every time a user moves the page.
     $(document).on("scroll", function() {
         // pageTop = how far down the page the user has scrolled (technically, finding the amount of pixels down the document the top of the browser currently is).
@@ -31,4 +83,5 @@ $(document).ready(function() {
             else { $(tag).removeClass("visible")}
         }
     })
+    
 });
